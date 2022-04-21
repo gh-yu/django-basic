e701177c7aebeb6797b148ff6c7921462b8e5695
gh-yu <buptl>
2022-04-21 15:05:16 +0900

    feat: 게시판 페이징 기능 추가

--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
 
 from board.forms import BoardForm
 from fcuser.models import Fcuser
@@ -42,6 +43,13 @@ def board_write(request):
     return render(request, 'board_write.html', {'form':form})
 
 def board_list(request):
-    boards = Board.objects.all().order_by('-id')
+    all_boards = Board.objects.all().order_by('-id')
+    page = request.GET.get('p', 1)
+    # Paginator(self, object_list, per_page, orphans=0,allow_empty_first_page=True)
+    # per_page : 한 페이지에 몇개 보여줄 것인지
+    paginator = Paginator(all_boards, 2)
+
+    boards = paginator.get_page(page) # get_page() 현재 페이지에 해당하는 boards 반환
+
     return render(request, 'board_list.html', {'boards': boards})
 
